911ad50d181da9e49ed32c9acb7f9da757afc3bb
oldArmenianMan <hassanmatGer226@gmail.com>
2024-09-03 17:37:31 +0300

    new endp

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -35,7 +35,7 @@ const NewsList = () =>
         const fetchMessages = async () =>
             {
                 try {
-                    const response = await axios.get('https://3c3faa853f13.vps.myjino.ru:3010/messages');
+                    const response = await axios.get('https://3c3faa853f13.vps.myjino.ru:49324/messages');
                     setMessages(response.data);
                     console.log("Ответ: ", response.data)
                 } catch (error) {
