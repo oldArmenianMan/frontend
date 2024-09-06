af20ef99a69843e1b1786b27c53863e00f4c87bb
oldArmenianMan <hassanmatGer226@gmail.com>
2024-09-06 12:37:48 +0300

    Тест для news list

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -31,13 +31,21 @@ const NewsList = () =>
 {
     const [messages, setMessages] = useState([])
     // const [photoUrl, setPhotoUrl] = useState([]);
+    const [photos, setPhotos] = useState([]);
+    const [videos, setVideos] = useState([]);
 
     useEffect(() => {
         const fetchMessages = async () =>
             {
                 try {
                     const response = await axios.get('https://xn--b1aahbbaz5a0afbu7i.su:49397/messages');
-                    setMessages(response.data.responseData);
+                    const { text, photo, video } = response.data;
+
+                    setMessages(text || []);
+                    setPhotos(photo || []);
+                    setVideos(video || []);
+
+                console.log("Ответ от API:", response.data);
                     // setPhotoUrl(response.data.responseData.photo);
                     console.log("Ответ: ", response.data.responseData)
                 } catch (error) {
@@ -53,11 +61,28 @@ const NewsList = () =>
             {messages.slice().reverse().map((message, index) =>
             <div key={index} className="newsItem container">
                 <ReadMore>
-                    {message.text}
+                    {message}
                 </ReadMore>
                 <img src={messages.photoUrl} alt="Фото"></img>
             </div>
         )}
+            
+            <h2>Фотографии</h2>
+            {photos.slice().reverse().map((photoUrl, index) => (
+                <div key={index}>
+                    <img src={photoUrl} alt={`Фото ${index}`} />
+                </div>
+            ))}
+            
+            <h2>Видео</h2>
+            {videos.slice().reverse().map((videoUrl, index) => (
+                <div key={index}>
+                    <video controls width="250">
+                        <source src={videoUrl} type="video/mp4" />
+                        Ваш браузер не поддерживает тег видео.
+                    </video>
+                </div>
+            ))}
         </>
     );
 };
